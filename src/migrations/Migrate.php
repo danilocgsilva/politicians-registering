5885618faf1a63a396b867ed3fe2cd83b41ad0fe
Danilo Silva <contact@danilocgsilva.me>
2022-08-05 13:01:05 -0300

    Some clean-up, migration class

--- src/migrations/Migrate.php
+++ src/migrations/Migrate.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Educacaopolitica\PoliticiansRegister;
+namespace Educacaopolitica\PoliticiansRegister\Migrations;
 
 class Migrate
-{
+{    
     public function migrate(): string
     {
         $migrationScript = file_get_contents(
