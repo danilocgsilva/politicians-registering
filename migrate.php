3ba2604968fdceb3d9ec5fb0af0a87258aa5a244
Danilo Silva <danilocgsilva@gmail.com>
2022-08-07 13:08:52 -0300

    Shrinked migrate php script

--- migrate.php
+++ migrate.php
@@ -1,17 +1,5 @@
 <?php
 
-require "vendor/autoload.php";
-
-use Danilocgsilva\PdoStringBuilder\Builder;
-use Educacaopolitica\PoliticiansRegister\Migrations\Migrate;
-
-$pdoString = (new Builder())
-    ->setDbDns(getenv("DATABASE_DNS"))
-    ->setDbName(getenv("DATABASE_NAME"))
-    ->getPdoString();
-
-$pdo = new PDO($pdoString, getenv("DATABASE_USER"), getenv("DATABASE_PASSWORD"));
-
-$migrate = new Migrate($pdo);
+require "src/migrations/migration_entry_includes.php";
 
 $migrate->migrate();
---
+++ src/migrations/migration_entry_includes.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . "/../../vendor/autoload.php";
+
+use Danilocgsilva\PdoStringBuilder\Builder;
+use Educacaopolitica\PoliticiansRegister\Migrations\Migrate;
+
+$pdoString = (new Builder())
+    ->setDbDns(getenv("DATABASE_DNS"))
+    ->setDbName(getenv("DATABASE_NAME"))
+    ->getPdoString();
+
+$pdo = new PDO($pdoString, getenv("DATABASE_USER"), getenv("DATABASE_PASSWORD"));
+
+$migrate = new Migrate($pdo);
