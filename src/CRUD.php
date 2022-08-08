4b5d3c6a87b48ecbc92d7a84b1bcb5bcd87e12e7
Danilo Silva <danilocgsilva@gmail.com>
2022-08-07 21:16:46 -0300

    CRUD and Politician object has been written

--- src/CRUD.php
+++ src/CRUD.php
@@ -8,6 +8,8 @@ use PDO;
 
 class CRUD
 {
+    private CONST TABLE_NAME = "politicians";
+    
     private PDO $pdo;
 
     public function __construct(PDO $pdo)
@@ -17,21 +19,44 @@ class CRUD
 
     public function create(Politician $politician)
     {
-
+        $createQuery = sprintf(
+            "INSERT INTO `%s` VALUES \"%s\";", 
+            self::TABLE_NAME, 
+            $politician->getName()
+        );
+        $this->pdo->prepare($createQuery);
+        $this->pdo->execute();
     }
 
-    public function read()
+    public function read(int $id): Politician
     {
-
+        $searchQuery = "SELECT name FROM `%s` WHERE id = ?;";
+        $resource = $this->pdo->prepare($searchQuery);
+        $resource->execute([$id]);
+        $result = $resource->fetch();
+        return (new Politician())
+            ->setName($result["name"])
+            ->setId($id);
     }
 
-    public function update()
+    public function update(int $id, string $name)
     {
-
+        $queryUpdate = "UPDATE ? SET name = \"?\" WHERE id = ?;";
+        $this->pdo->prepare($queryUpdate);
+        $this->pdo->execute([
+            self::TABLE_NAME,
+            $name,
+            $id
+        ]);
     }
 
-    public function delete()
+    public function delete(int $id)
     {
-
+        $queryDelete = "DELETE FROM ? WHERE id = ?;";
+        $this->pdo->prepare($queryDelete);
+        $this->pdo->execute([
+            self::TABLE_NAME,
+            $id
+        ]);
     }
 }
--- src/Politician.php
+++ src/Politician.php
@@ -6,6 +6,7 @@ namespace Educacaopolitica\PoliticiansRegister;
 
 class Politician
 {
+    private int $id;
     private string $name;
 
     public function setName(string $name): self
@@ -17,5 +18,16 @@ class Politician
     public function getName(): string
     {
         return $this->name;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getid(): int
+    {
+        return $this->id;
     }
 }
